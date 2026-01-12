0853c498b9660f6b235649b01d9d559fce27162e
csz4621 <csz4621@naver.com>
2026-01-12 22:54:31 +0900

    테스트

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,17 +1,22 @@
 import coverImage from "./cover.webp"
 import placeHolderImage from "./placeholder.webp"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import image1 from "./CBY-5800.jpg"
+import image2 from "./CBY-5817-.jpg"
+import image3 from "./CBY-5840-.jpg"
+import image4 from "./CBY-5870-.jpg"
+import image5 from "./CBY-6013-.jpg"
+import image6 from "./CBY-6026-.jpg"
+import image7 from "./CBY-6081-.jpg"
+import image8 from "./CBY-6124-.jpg"
+import image9 from "./CBY-6186-.jpg"
+import image10 from "./CBY-6268-.jpg"
+import image11 from "./CBY-6294-.jpg"
+import image12 from "./CBY-6330-.jpg"
+import image13 from "./CBY-6387-.jpg"
+import image14 from "./CBY-6548-.jpg"
+import image15 from "./CBY-6764.jpg"
+import image16 from "./CBY-6765.jpg"
+import image17 from "./CBY-6766.jpg"
 
 export const COVER_IMAGE = coverImage
 export const PLACEHOLDER_IMAGE = placeHolderImage
@@ -29,4 +34,9 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17
 ]
